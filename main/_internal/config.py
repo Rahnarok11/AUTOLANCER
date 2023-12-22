61b1c8cf5c85914e26325098ed04df0c57d5d08b
Leonardo Capistrano <leonardo.capistrano@a.catolica-to.edu.br>
2023-12-22 10:40:53 -0300

    Adicionando versão 1.4

---
+++ main/_internal/config.py
@@ -0,0 +1,16 @@
+#Pastas
+caminho_pasta_complemento = r"\\10.0.0.252\arquivos\LANCAMENTO\COMPLEMENTO_NFS-e"
+caminho_pasta_xml = r"\\10.0.0.252\arquivos\LANCAMENTO\NFS-e"
+caminho_pasta_xlsx = r"\\10.0.0.252\arquivos\LANCAMENTO\RATEIO_NFS-e"
+caminho_tema_azure = r"\\10.0.0.252\arquivos\LANCAMENTO\TEMA_AZURE"
+
+#Icones
+caminho_icone_trator = r"\\10.0.0.252\arquivos\LANCAMENTO\ICONE\Trator.ico"
+caminho_icone_pasta = r"\\10.0.0.252\arquivos\LANCAMENTO\ICONE\Pasta.ico"
+
+#caminho_pasta_complemento = r"C:\Users\AutoLanc\Documents\TESTE\PRINCIPAL\COMPLEMENTO_NFS_e"
+#caminho_pasta_xml = r"C:\Users\AutoLanc\Documents\TESTE\PRINCIPAL\NFS_e"
+#caminho_pasta_xlsx = r"C:\Users\AutoLanc\Documents\TESTE\PRINCIPAL\RATEIO_NFS_e"
+#caminho_icone_trator = r"C:\Users\AutoLanc\Documents\TESTE\BD_LOCAL\ICONE\Trator.ico"
+#caminho_icone_pasta = r"C:\Users\AutoLanc\Documents\TESTE\BD_LOCAL\ICONE\Pasta.ico"
+#caminho_azure_tema = r"C:\Users\AutoLanc\Documents\tema\Azure-ttk-theme\azure.tcl"
